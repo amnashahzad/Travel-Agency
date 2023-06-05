808a9184b0c316c8756ce79d1e5dc3940e855cb2
amnashahzad <121329321+amnashahzad@users.noreply.github.com>
2023-06-05 08:19:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function register() {
+    event.preventDefault(); // Prevent form submission
+
+    // Retrieve form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Perform form validation
+
+    // Perform registration
+
+    // Display success message or redirect to another page
+    alert("Registration successful!");
+}
